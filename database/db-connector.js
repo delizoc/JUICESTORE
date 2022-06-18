feedca8e52ae6b1120915c1d6b1d618b1450256d
Colette DeLizo <delizoc@oregonstate.edu>
2022-06-18 14:38:36 -0700

    connected to db. realized still need total price table.

--- database/db-connector.js
+++ database/db-connector.js
@@ -6,10 +6,10 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_delizoc',
-    password        : '2857',
-    database        : 'cs340_delizoc'
+    host            : 'localhost',
+    user            : 'root',
+    password        : '',
+    database        : 'juice'
 })
 
 // Export it for use in our applicaiton
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 setup:
-	mysql -u cs340_delizoc -h classmysql.engr.oregonstate.edu -p
+	mysql -u juice -h root 
 password: 2857
 	USE cs340_delizoc
 	source ./juiceStore.sql;
